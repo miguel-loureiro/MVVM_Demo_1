e74c57debffda69b066d63aa4c09cd3e2c2ed77b
Miguel Loureiro <miguel.silvaloureiro@gmail.com>
2023-08-07 10:57:31 +0100

    initial project structure

---
+++ MVVM-demo-1/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  MVVM-demo-1
+//
+//  Created by António Loureiro on 07/08/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .blue
+        // Do any additional setup after loading the view.
+    }
+}
+
